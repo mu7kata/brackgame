59355d0dd38cf948dfd9792163b4bdd91332583d
mu7kata <mu7kata@icloud.com>
2020-08-23 14:56:40 +0900

    css追加

--- index.php
+++ index.php
@@ -19,21 +19,29 @@ ini_set("display_errors", "On");
 
 <!DOCTYPE html>
 <html>
+    
  <head>
         <meta charset="utf-8">
         <title>ブラック企業からの脱出</title>
         <link rel="stylesheet" type="text/css" href="style.css">
  </head>
+    <h2>ゲーム：ブラック企業からの脱出</h2>
   <body>
-   <h1></h1>
-   <div>
-   <img src="" alt="">
-   </div>
-   <p></p>
+   
+      <div class="main">
+          <h1>テスト</h1>
+          <img src="img/sekuhara.jpeg" style="padding-left:163px;">
+ 
+      <p>脱出まであと：<?php ?>/5人</p>
+   <p>HP：<?php ?>/500</p>
+   <div class="main_command">
    <p></p>
    <form action="post">
-      
+      <input type="submit" name='attack' value='▶︎攻撃する'>
+      <input type="submit" name='escape' value='▶︎逃げる'>
+       <input type="submit" name='next' value='▶︎次へ'>
    </form>
-  
+          </div>
+      </div>
   </body>
 </html>
